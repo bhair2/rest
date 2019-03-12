9c589709bdc529cadb8d2155e2567918ed42ff06
balazsj <user@server.com>
2019-03-12 15:53:04 +0100

    c3

--- horoscope.c
+++ horoscope.c
@@ -5,8 +5,8 @@ void printHoroscopeSign(int month, const char *text)
   print("%d%s", month, text);
   switch(month)
   {
-      case 0: print("Capricorn"); break;
-      case 1: print("Aquarius"); break;
+      case 0: print("Dragon"); break;
+      case 1: print("Horse"); break;
       default: print("not implemented");
   }
 }
